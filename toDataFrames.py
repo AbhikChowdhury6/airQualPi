9bacca7a9e0e24a77507662c8fc98d3dbedc004b
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-04-09 10:35:11 -0700

    misspelled source folder

--- toDataFrames.py
+++ toDataFrames.py
@@ -3,7 +3,7 @@
 import os
 import csv
 import pandas as pd
-source = "/home/" + os.getlogin() + "/Documents/daysData/"
+source = "/home/" + os.getlogin() + "/Documents/dayData/"
 
 destination = "/home/" + os.getlogin() + "/Documents/sensorData/"
 
