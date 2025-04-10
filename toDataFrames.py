9eceba7aca666234004da2d0e0a0213131db786f
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-04-09 22:48:52 -0700

    i think this format specifier is the play

--- toDataFrames.py
+++ toDataFrames.py
@@ -46,7 +46,7 @@ for file in csvs:
 
     #set the index as sampleDT
     df = df.set_index(headers[0])
-    df.index = pd.to_datetime(df.index, utc=True)
+    df.index = pd.to_datetime(df.index, utc=True, format='ISO8601')
     ic(df.dtypes)
     ic(df.head())
     # each file is named with the first and last datetime
